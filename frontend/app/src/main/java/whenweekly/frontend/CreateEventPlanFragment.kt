cc37aaf0dda752f036bc91abd1785d76b1800515
Sang Nguyen <sangnn@stud.ntnu.no>
2022-10-31 16:23:54 +0100

    Added implementation for creating an event

--- frontend/app/src/main/java/whenweekly/frontend/CreateEventPlanFragment.kt
+++ frontend/app/src/main/java/whenweekly/frontend/CreateEventPlanFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.datepicker.MaterialDatePicker
+import whenweekly.frontend.databinding.FragmentCreateEventPlanBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +21,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CreateEventPlanFragment : Fragment() {
+    // assign the _binding variable initially to null and
+    // also when the view is destroyed again it has to be set to null
+    private var _binding: FragmentCreateEventPlanBinding? = null
+    // with the backing property of the kotlin we extract
+    // the non null value of the _binding
+    private val binding get() = _binding!!
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -26,15 +36,53 @@ class CreateEventPlanFragment : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+        }
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event_plan, container, false)
+    ):  View? {
+        // inflate the layout and bind to the _binding
+        _binding = FragmentCreateEventPlanBinding.inflate(inflater, container, false)
+
+        binding.selectDates.setOnClickListener {
+            showDataRangePicker()
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showDataRangePicker() {
+        val dateRangePicker = MaterialDatePicker
+            .Builder.dateRangePicker()
+            .setTitleText("Select Date")
+            .setTheme(R.style.MaterialDatePickerTheme)
+            .build()
+
+        dateRangePicker.show(parentFragmentManager, "date_range_picker")
+
+        dateRangePicker.addOnPositiveButtonClickListener { dateSelected ->
+
+            val startDate = dateSelected.first
+            val endDate = dateSelected.second
+
+            if (startDate != null && endDate != null) {
+                binding.tvRangeDate.text = "Reserved\nStartDate: ${convertLongToTime(startDate)}\n" + "EndDate: ${convertLongToTime(endDate)}"
+            }
+        }
+    }
+
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm")
+        return format.format(date)
     }
 
     companion object {
