14756451f6463b83f75f0d2587c609b06b6a8628
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-11-07 19:23:12 +0100

    Add integrated entity serialization to backend

--- backend/src/main/kotlin/whenweekly/plugins/Serialization.kt
+++ backend/src/main/kotlin/whenweekly/plugins/Serialization.kt
@@ -1,12 +1,28 @@
 package whenweekly.plugins
 
-import io.ktor.serialization.kotlinx.json.*
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import org.ktorm.jackson.KtormModule
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Application.configureSerialization() {
-    // Configure ktormmodule for jackson
     install(ContentNegotiation) {
-        json()
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+            // ktorm entity serialize support
+            registerModule(KtormModule())
+            // java LocalDateTime serialize support
+            registerModule(JavaTimeModule().apply {
+                addSerializer(
+                    LocalDateTime::class.java,
+                    LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+                )
+            })
+        }
     }
 }
