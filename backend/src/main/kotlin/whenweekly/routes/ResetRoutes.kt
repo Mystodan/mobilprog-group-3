f8c370f4c0f930820e0989277dd94dd980a1b986
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-11-19 16:50:40 +0100

    Use constant for event route

--- backend/src/main/kotlin/whenweekly/routes/ResetRoutes.kt
+++ backend/src/main/kotlin/whenweekly/routes/ResetRoutes.kt
@@ -8,13 +8,14 @@ import io.ktor.server.routing.*
 import whenweekly.database.DatabaseManagerImpl
 import whenweekly.domain.manager.DatabaseManager
 import whenweekly.routes.Constants.BUILD_CONFIG
+import whenweekly.routes.Constants.RESET_ROUTE
 
 fun Route.resetRouting() {
     val database: DatabaseManager = DatabaseManagerImpl()
 
     // Only for development
     if (environment?.config?.property(BUILD_CONFIG)?.getString().equals("dev", true)){
-        route("/reset") {
+        route(RESET_ROUTE) {
             resetDatabase(database)
         }
     }
